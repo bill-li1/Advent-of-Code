1b92b181eb83d1a7cbea45bc5ddf7137c300dd3b
Bill Li <billli@gmail.com>
2024-01-04 19:29:45 -0800

    Advent

--- 2023/21/a.cpp
+++ 2023/21/a.cpp
@@ -1,71 +1,77 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 const int num = 64;
+const int test = 0;
 
 int main() {
-    string temp;
-    vector<string> m;
-    while(cin >> temp) {
-        m.push_back(temp);
+  string temp;
+  vector<string> m;
+  while (cin >> temp) {
+    m.push_back(temp);
+  }
+  int startr, startc;
+  int rows = m.size();
+  int cols = m[0].size();
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < cols; c++) {
+      if (m[r][c] == 'S') {
+        startr = r;
+        startc = c;
+      }
     }
-    int startr, startc;
-    int rows = m.size();
-    int cols = m[0].size();
-    for(int r=0; r<rows; r++) {
-        for(int c=0; c<cols; c++) {
-            if(m[r][c] == 'S') {
-                startr = r;
-                startc = c;
-            }
-        }
+  }
+  vector<vector<int>> step(rows, vector<int>(cols, -1));
+  queue<int> rowq;
+  queue<int> colq;
+  rowq.push(startr);
+  colq.push(startc);
+  step[startr][startc] = 0;
+  while (!rowq.empty() && !colq.empty()) {
+    int r = rowq.front();
+    int c = colq.front();
+    rowq.pop();
+    colq.pop();
+    if (r - 1 >= 0 && m[r - 1][c] != '#' && step[r - 1][c] < 0 &&
+        step[r][c] < num) {
+      step[r - 1][c] = step[r][c] + 1;
+      rowq.push(r - 1);
+      colq.push(c);
     }
-    vector<vector<int>> step(rows, vector<int>(cols, -1));
-    queue<int> rowq;
-    queue<int> colq;
-    rowq.push(startr);
-    colq.push(startc);
-    step[startr][startc] = 0;
-    while(!rowq.empty() && !colq.empty()) {
-        int r = rowq.front();
-        int c = colq.front();
-        rowq.pop();
-        colq.pop();
-        if(r-1 >= 0 && m[r-1][c] != '#' && step[r-1][c] < 0 && step[r][c] < num) {
-            step[r-1][c] = step[r][c] + 1;
-            rowq.push(r-1);
-            colq.push(c);
-        }
-        if(r+1 < rows && m[r+1][c] != '#' && step[r+1][c] < 0 && step[r][c] < num) {
-            step[r+1][c] = step[r][c] + 1;
-            rowq.push(r+1);
-            colq.push(c);
-        }
-        if(c-1 >= 0 && m[r][c-1] != '#' && step[r][c-1] < 0 && step[r][c] < num) {
-            step[r][c-1] = step[r][c] + 1;
-            rowq.push(r);
-            colq.push(c-1);
-        }
-        if(c+1 < cols && m[r][c+1] != '#' && step[r][c+1] < 0 && step[r][c] < num) {
-            step[r][c+1] = step[r][c] + 1;
-            rowq.push(r);
-            colq.push(c+1);
-        }
+    if (r + 1 < rows && m[r + 1][c] != '#' && step[r + 1][c] < 0 &&
+        step[r][c] < num) {
+      step[r + 1][c] = step[r][c] + 1;
+      rowq.push(r + 1);
+      colq.push(c);
     }
-    int count = 0;
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<cols; j++) {
-            if(step[i][j] % 2 == 0) count++;
-            if(step[i][j] == -1) {
-                cout << "X ";
-            } else {
-                cout << step[i][j] << " ";
-            }
-        }
-        cout << endl;
+    if (c - 1 >= 0 && m[r][c - 1] != '#' && step[r][c - 1] < 0 &&
+        step[r][c] < num) {
+      step[r][c - 1] = step[r][c] + 1;
+      rowq.push(r);
+      colq.push(c - 1);
     }
-    cout << count << endl;
+    if (c + 1 < cols && m[r][c + 1] != '#' && step[r][c + 1] < 0 &&
+        step[r][c] < num) {
+      step[r][c + 1] = step[r][c] + 1;
+      rowq.push(r);
+      colq.push(c + 1);
+    }
+  }
+  int count = 0;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (step[i][j] % 2 == 0)
+        count++;
+      if (step[i][j] == -1) {
+        cout << "X ";
+      } else {
+        cout << step[i][j] << " ";
+      }
+    }
+    cout << endl;
+  }
+  cout << count << endl;
 }
