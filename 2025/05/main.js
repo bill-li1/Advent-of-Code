6ace050ea84f2d0fe19bff0b4f4a9ad70bd4212e
Bill Li <billli@gmail.com>
2026-01-27 14:43:40 -0800

    trying part 2 day 6

--- 2025/05/main.js
+++ 2025/05/main.js
@@ -3,59 +3,16 @@ import Bun from 'bun'
 const testFile = Bun.file('./sample.txt')
 const realFile = Bun.file('./input.txt')
 
-// const data = (await testFile.text()).trim()
-const data = (await realFile.text()).trim()
+const data = (await testFile.text()).trim()
+// const data = (await realFile.text()).trim()
 
 function partOne() {
   const ranges = []
-  let count = 0
   data.split(`\n`).forEach((line) => {
     if (line.includes('-')) {
-      const [start, end] = line.split('-')
-      ranges.push([Number(start), Number(end)])
-    } else {
-      const cur = Number(line)
-      for (const [start, end] of ranges) {
-        if (cur >= start && cur <= end) {
-          count++
-          break
-        }
-      }
+      ranges.push([])
     }
   })
-  console.log(count)
 }
 
-function partTwo() {
-  let currentRange = []
-  let count = 0
-  const dataArr = data.split(`\n`)
-  const ranges = []
-  for (const line of dataArr) {
-    if (line.includes('-')) {
-      const [start, end] = line.split('-')
-      ranges.push([Number(start), Number(end)])
-    } else {
-      break
-    }
-  }
-  ranges.sort((a, b) => {
-    return a[0] >= b[0]
-  })
-  for (const range of ranges) {
-    if (!currentRange.length) {
-      currentRange = range
-      continue
-    }
-    if (range[0] <= currentRange[1]) {
-      currentRange[1] = Math.max(currentRange[1], range[1])
-    } else {
-      count += currentRange[1] - currentRange[0] + 1
-      currentRange = range
-    }
-  }
-  count += currentRange[1] - currentRange[0] + 1
-  console.log(count)
-}
-
-partTwo()
+partOne()
